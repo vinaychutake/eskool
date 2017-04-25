30ae4ab4db2941aa169d6ce393e2276fb334c8fd
Vinay Chutake <vnyctk26@gmail.com>
2017-04-25 12:23:18 +0530

    auth pages style changes, forgot password flow and styles

--- auth_management/admin.py
+++ auth_management/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from auth_management import models
 # Register your models here.
+
+admin.site.register(models.User)
--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,7 @@
 """
 Utility functions
 """
+import re
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -19,3 +20,12 @@ def paginate_objects(objects, objects_per_page, page_no=None):
         page_objects = paginator.page(paginator.num_pages)
 
     return page_objects
+
+
+def check_email_validity(email):
+    email_regex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
+    match = re.match(email_regex, email)
+    if match == None:
+        return False
+    else:
+        return True
